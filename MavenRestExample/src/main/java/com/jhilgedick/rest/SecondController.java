93c9ee331804c81c4cfbfc359f82fce66199d2d7
Hilgedick, John <John.Hilgedick@disney.com>
2017-04-24 10:51:21 -0700

    added test

--- MavenRestExample/src/main/java/com/jhilgedick/rest/SecondController.java
+++ MavenRestExample/src/main/java/com/jhilgedick/rest/SecondController.java
@@ -7,9 +7,20 @@ import javax.ws.rs.core.Response;
 
 @Path("/second")
 public class SecondController {
+
 	@GET
-	@Path("/{param}")
-	public Response getMsg(@PathParam("param") String msg) {
+	@Path("/a/{param}")
+	public Response getMsgA(@PathParam("param") String msg) {
+
+		String output = "Jersey say : " + msg;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+	@GET
+	@Path("/b/{param}")
+	public Response getMsgB(@PathParam("param") String msg) {
 
 		String output = "Jersey say : " + msg;
 
---
+++ MavenRestExample/src/test/com/jhilgedick/rest/TestSecondController.java
@@ -0,0 +1,28 @@
+package com.jhilgedick.rest;
+
+import javax.ws.rs.core.Response;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestSecondController {
+
+	@Test
+	public void testSecondControllerA() {
+		SecondController c = new SecondController();
+		Response r = c.getMsgA("johnny boy!");
+		assertNotNull(r);
+		assertEquals(200, r.getStatus());
+		System.out.println("hi from A");
+	}
+
+	@Test
+	public void testSecondControllerB() {
+		SecondController c = new SecondController();
+		Response r = c.getMsgB("johnny boy!");
+		assertNotNull(r);
+		assertEquals(400, r.getStatus());
+		System.out.println("hi from A");
+	}
+}
