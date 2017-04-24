d516d5b5811dc32070c1bdc41bab705cc4e63572
Hilgedick, John <John.Hilgedick@disney.com>
2017-04-24 16:22:08 -0700

    tweak

--- MavenRestExample/src/test/java/com/jhilgedick/rest/TestSecondController.java
+++ MavenRestExample/src/test/java/com/jhilgedick/rest/TestSecondController.java
@@ -32,6 +32,6 @@ public class TestSecondController {
 		Response r = c.getMsgB("johnny boy!");
 		assertNotNull(r);
 		assertEquals(200, r.getStatus());
-		System.out.println("hi from F");
+		System.out.println("hi from G");
 	}
 }
